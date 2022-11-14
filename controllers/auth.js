7d8072acf19e78361b162cf523f0b8a0876bf3b5
Kender ROMAIN <kenderromain@gmail.com>
2022-11-13 20:48:37 -0500

    Base Login Feature added

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,7 +29,21 @@ export const register = (req, res) => {
 };
 
 export const login = (req, res) => {
+  //Check if the username exists
+  const q = "SELECT * FROM users WHERE username=?";
+
+  db.query(q, [req.body.username], (error, data) => {
+    if (error) return res.status(500).json(error);
+    if (data.length === 0) return res.status(404).json("User not found");
+
+    const userPasswordDB = data[0].password;
+    console.log({ userPasswordDB });
+    //Check if the password is correct
+    const checkPassword = bcrypt.compareSync(req.body.password, userPasswordDB);
+    if (checkPassword) return res.status(200).json(data);
+
+    return res.status(404).json("The password or username is incorrect");
+  });
 };
 
-export const logout = (req, res) => {
-};
+export const logout = (req, res) => {};
