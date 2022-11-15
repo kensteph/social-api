602062653cbf64990bebcaa8a7b36357df6b6fb1
Kender ROMAIN <kenderromain@gmail.com>
2022-11-14 23:32:38 -0500

    Adding security to Login Feature by using JWSONWEBTOKEN,CORS & COOKIE-PARSER. Adding also the Logout Feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db/connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //Check if the user exists
@@ -40,10 +41,26 @@ export const login = (req, res) => {
     console.log({ userPasswordDB });
     //Check if the password is correct
     const checkPassword = bcrypt.compareSync(req.body.password, userPasswordDB);
-    if (checkPassword) return res.status(200).json(data);
+    if (!checkPassword)
+      return res.status(404).json("The password or username is incorrect");
 
-    return res.status(404).json("The password or username is incorrect");
+    //Destructuration of data in order to avoid sending the user's password
+    const {password ,...others} = data[0]
+
+    //Secure the app by sending a token to the authenticared user
+    //First create the token
+    const token = jwt.sign({ id: data[0].id }, "SecretKey8788");
+
+    //Send the cookie
+    res.cookie("accessToken", token, {
+      httpOnly: true,//Mean we can only use this token through the web
+    }).status(200).json(others);
   });
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res.clearCookie("accessToken",{
+    secure : true,
+    sameSite: "none"
+  }).status(200).json("User has been logged out")
+};
